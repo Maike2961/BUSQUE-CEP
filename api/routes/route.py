022a5d6f0b43c7aa8c7186cc81003c11dca750ff
Maike2961 <mayki.silva@hotmail.com>
2024-07-10 16:53:58 -0300

    adicionado def dictas

--- api/routes/route.py
+++ api/routes/route.py
@@ -5,19 +5,10 @@ from api import app
 
 @app.route("/")
 def index():
-    
     return redirect("/busca")
 
 @app.route("/busca", methods=["GET", "POST"])
 def busca_cep():
-    content = {
-        "rua": "",
-        "bairro": "",
-        "cidade": "",
-        "uf": "",
-        "cep": "",
-        "ddd": ""
-    }
     if request.method == "POST":
         cep = request.form["cep"]
         link_cep = f'https://viacep.com.br/ws/{cep}/json/'
@@ -25,14 +16,7 @@ def busca_cep():
         if(len(cep) == 8):
             try:
                 dicta = resp.json()
-                content = {
-                    "rua": dicta['logradouro'],
-                    "bairro": dicta['bairro'],
-                    "cidade": dicta['localidade'],
-                    "uf": dicta['uf'],
-                    "cep": dicta['cep'],
-                    "ddd": dicta['ddd']
-                }
+                content =  dictas(dicta['logradouro'], dicta['bairro'],  dicta['localidade'], dicta['uf'],  dicta['cep'],dicta['ddd'])
                 print(content)
             except Exception as e:
                 print(f"Erro na requisição {e}")
@@ -42,4 +26,24 @@ def busca_cep():
             flash("Erro tente novamente", 'danger')
     else:
         print("Erro de conexão")
+        content = {
+            "rua": "",
+            "bairro": "",
+            "cidade": "",
+            "uf": "",
+            "cep": "",
+            "ddd": ""
+        }
     return render_template('index.html', content=content)
+
+
+def dictas(rua, bairro, cidade, uf, cep,ddd):
+    content = {
+        "rua": rua,
+        "bairro": bairro,
+        "cidade": cidade,
+        "uf": uf,
+        "cep": cep,
+        "ddd": ddd
+    }
+    return content
